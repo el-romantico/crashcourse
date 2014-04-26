e70359e01d8918222ce97f1a7623d68ae31614ed
Alexander Ivanov <alehander42@gmail.com>
2014-04-26 22:46:19 +0300

    Display courses for which an user is enrolled in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find params[:id]
-    @courses = Course.where(lecturer_id: @user.id) || []
+    @courses_as_teacher = Course.where(lecturer_id: @user.id) || []
     @requests = @user.requests || []
   end
 end
